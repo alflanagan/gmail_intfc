084a81872fda007fd47202b66124fc9278fcd60a
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2017-02-17 17:48:48 -0500

    initial commit

---
+++ build/index.js
@@ -0,0 +1,2 @@
+"use strict";var _mailbox=require("./mailbox");let a=new _mailbox.Mailbox;a.fred();
+//# sourceMappingURL=index.js.map
---
+++ src/mailbox.js
@@ -0,0 +1,8 @@
+// uses babel features through babel-preset-es2017
+
+export class Mailbox {
+
+    async fred() {
+      console.log("fred")
+    }
+}
